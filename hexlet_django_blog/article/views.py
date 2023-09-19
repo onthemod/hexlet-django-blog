6fad1383370511bb1df941ea4f8d6f242c5fd9c1
onthemod <onthemod@yandex.ru>
2023-09-20 01:30:16 +0300

    dynamic routing with reversed urls added

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.views import View
+from django.http import HttpResponse
+
+def index(request, tags, article_id):
+    return HttpResponse(f'Статья номер {article_id}. Тег {tags}') 
+    #context = {'app_name': 'Блог имени Хекслета'}
+    #return render(request, 'articles/index.html', context) 
 
 # Create your views here.
 class ArticleIndexView(View):
--- hexlet_django_blog/views.py
+++ hexlet_django_blog/views.py
@@ -1,13 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.base import TemplateView
+from django.urls import reverse
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+    
+    def get(self, request, *args, **kwargs):
+        url = reverse('article', kwargs={'article_id': 42, 'tags': 'python'})
+        return redirect(url)
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['who'] = 'World'
-        return context
+    #def get_context_data(self, **kwargs):
+    #   url = reverse('article', kwargs={'article_id': 42, 'tags': 'python'})
+    #   return redirect(url)
 		
 def about(request):
     return render(request, 'about.html')
