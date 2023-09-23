b006644021da0b4a70295c83759c6b67bf57f337
onthemod <onthemod@yandex.ru>
2023-09-23 22:34:54 +0300

    added lings to articles from article index page

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -17,9 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from hexlet_django_blog.article import views
-from hexlet_django_blog.article.views import IndexView 
+from hexlet_django_blog.article.views import IndexView, ArticleView
 
 urlpatterns = [
 	path('', IndexView.as_view(), name='articles'),
-	path('<str:tags>/<int:article_id>/', views.index, name='article'),
+	path('<int:id>/', ArticleView.as_view(), name='article'),
 ]
