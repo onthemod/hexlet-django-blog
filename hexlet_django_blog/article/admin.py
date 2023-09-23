bc4a6d10d2a55a9f0ee4458d9675bd0b8f2094ce
onthemod <onthemod@yandex.ru>
2023-09-23 13:12:25 +0300

    admin page tuning

--- hexlet_django_blog/article/admin.py
+++ hexlet_django_blog/article/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.admin import DateFieldListFilter
+
+from .models import Article
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('name', 'timestamp')
+    search_fields = ['name', 'body']
+    list_filter = (('timestamp', DateFieldListFilter),)
+
 
 # Register your models here.
